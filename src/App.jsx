58193acf511aec22e2431b33eae498157dac34b8
hiep1997xz <73546080+hiep1997xz@users.noreply.github.com>
2022-02-16 20:51:05 +0700

    feature-home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </>
   );
 }
 
